2cb888371196327603806ffeb12034f522e49b13
Anto Ter-Mesropian <antotm99@gmail.com>
2021-03-02 20:15:34 +0000

    added exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -15,3 +15,18 @@ puts "----------"
 # 2. Using the `where` class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable `@mens_stores`.
 # 3. Loop through each of these stores and output their name and annual revenue on each line.
 # 4. Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+
+Store.create([
+  { name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true},
+  { name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false},
+  { name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true}
+])
+
+@mens_stores = Store.where("mens_apparel = true")
+
+@mens_stores.each { |x| puts "#{x.name} Store has #{x.annual_revenue} revenue." }
+
+@womens_stores = Store.where(["womens_apparel = ? and annual_revenue > ?", "true", "1000000"])
+
+@womens_stores.each { |x| puts "#{x.name} Store has #{x.annual_revenue} revenue." }
+
