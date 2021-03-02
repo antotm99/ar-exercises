58eb7cae5555f742641fdc1a80ab1125281450fe
Anto Ter-Mesropian <antotm99@gmail.com>
2021-03-02 20:02:41 +0000

    fixed Store.create

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -11,7 +11,7 @@ puts "----------"
 #  * Gastown (annual_revenue of 190000 carries men's apparel only)
 # 2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the database.
 
-Stroe.create([
+Store.create([
   { name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true},
   { name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true},
   { name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false}
